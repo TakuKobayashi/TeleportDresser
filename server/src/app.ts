bac301e2fb8c36a1979c5d1e8ee271878fdb4319
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-05-24 12:20:21 +0900

    image listを取得するAPIの作成

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,6 +18,25 @@ app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
 
+app.get('/images', async (req, res) => {
+  const fireStorage = setupFireStorage();
+  const fileList = [];
+  const fileBuckets = await fireStorage.bucket().getFiles();
+  for(const files of fileBuckets){
+    for(const file of files){
+      fileList.push({
+        image_id: file.metadata.id,
+        image_url: "https://firebasestorage.googleapis.com/v0" + file.parent.baseUrl + "/" + file.parent.id + file.baseUrl + "/" + file.id + "?alt=media",
+        content_type: file.metadata.contentType,
+        file_size: file.metadata.size,
+        created_at: file.metadata.timeCreated,
+        updated_at: file.metadata.updated,
+      })
+    }
+  }
+  res.json(fileList);
+});
+
 app.get('/upload', (req, res) => {
   const fireBucket = setupFireStorage().bucket();
   fireBucket.upload('./onepiece_ex01.png', { destination: 'aaa.png' }).then((file) => {
