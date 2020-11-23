7c2718574eaba8868dc8e44811ad304c76d41060
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-11-23 11:01:21 +0900

    firestoreに販売するもののデータを追加した

--- server/src/common/firebase-storage.ts
+++ server/src/common/firebase-storage.ts
@@ -1,12 +1,13 @@
-import { setupFireStorage } from './firebase';
+import { setupFireStorage, setupFireStore } from './firebase';
 
 export async function getFileList(): Promise<any[]> {
   const fireStorage = setupFireStorage();
-  const fileList = [];
+  const fileIdList = {};
+  const fileListPromises = [];
   const fileBuckets = await fireStorage.bucket().getFiles();
   for(const files of fileBuckets){
     for(const file of files){
-      fileList.push({
+      const fileMeta = {
         name: file.id,
         price: 1000,
         currency: "JPY",
@@ -16,8 +17,25 @@ export async function getFileList(): Promise<any[]> {
         file_size: file.metadata.size,
         created_at: file.metadata.timeCreated,
         updated_at: file.metadata.updated,
-      })
+      }
+      fileListPromises.push(updateOrGetFileMeta(fileMeta));
     }
   }
-  return fileList;
+  const fileMetaList = await Promise.all(fileListPromises);
+  for(const fileMeta of fileMetaList){
+    fileIdList[fileMeta.name] = fileMeta;
+  }
+  return Object.values(fileIdList);
+}
+
+async function updateOrGetFileMeta(fileMeta: any){
+  const firestore = setupFireStore();
+  const productMeta = await firestore.collection("Products").doc(fileMeta.name).get()
+  const productMetaData = productMeta.data();
+  if(productMetaData){
+    return productMetaData;
+  } else {
+    const result = await firestore.collection("Products").doc(fileMeta.name).set(fileMeta)
+    return fileMeta;
+  }
 }
